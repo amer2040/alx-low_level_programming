d2cd5ec88b36f92b8719aef07b2517bd225166d6
Abdelrahman <aboodamer7@gmail.com>
2023-06-26 05:00:47 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes aponter to an inr as parameter
+ * and update the value to 98
+ *
+ * @n: int parameter
+ *
+ * return: void
+*/
+
+void reset_to_98(int n*)
+{
+	*n = 98;
+}
