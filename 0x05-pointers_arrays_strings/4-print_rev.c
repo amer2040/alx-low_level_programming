1e06d92654a67a2d6ffba3712f44ca6f5043c367
Abdelrahman <aboodamer7@gmail.com>
2023-06-26 05:30:59 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed
+ *
+ * @s: pointer to the strig to print
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (s[r])
+		r++;
+
+	while (r++)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}
